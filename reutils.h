5470a6b9f834f4ff5f195ea1a0df914a76b6af65
Félix <142559040+filouRE@users.noreply.github.com>
2023-08-28 14:58:41 -0400

    started to work on string and some constants

--- reutils.h
+++ reutils.h
@@ -47,9 +47,9 @@ namespace Utils
             m_size = data.m_size;
             m_data = std::make_unique<char[]>(m_size);
             
-            for (int character{0}; character < character; it++)
+            for (int character{0}; character < character; character++)
             {
-                m_data[it] = data.m_data[it];
+                m_data[character] = data.m_data[character];
             }
             
             return *this;
@@ -128,6 +128,105 @@ namespace Utils
     namespace Math
     {
         // TODO - Add more math functions && constants
+
+        namespace Constants
+        {
+            constexpr double PI = 3.14159265358979323846;
+            constexpr double EULERS = 2.71828182845904523536;
+            constexpr double GOLDEN_RATIO = 1.61803398874989484820;
+            constexpr int SPEED_OF_LIGHT = 186282;            
+        }
+
+        template <typename T, typename U>
+        class Vector2
+        {
+        public:
+            /* CONSTRUCTORS */
+            Vector2() 
+            {
+                this->m_x = 0;
+                this->m_y = 0;
+            }
+
+            Vector2(T x)
+                : m_x{ x }, m_y{ 0 }
+            {}
+
+            Vector2(T x, U y)
+                : m_x{ x }, m_y{ y }
+            {}
+
+            /* DESTRUCTORS */
+            ~Vector2() = delete;
+
+            // Copy constructor
+            Vector2(const Vector2& obj)
+                : m_x{ obj.m_x }, m_y{ obj.m_y }
+            {}
+
+            // Copy assignment
+            Vector2& operator=(const Vector2& obj)
+            {
+                if (this == &data)
+                    return *this;
+                
+                this->m_x = obj.m_x;
+                this->m_y = obj.m_y;
+
+                return *this;
+            }
+
+            // Move constructor
+            Vector2(Vector&& obj) noexcept
+                : m_x{ std::move(obj.m_x) }, m_y{ std::move(obj.m_y) }
+            {
+                obj.m_x = 0;
+                obj.m_y = 0;
+            }
+
+            // Move assignment
+            Vector2& operator=(Vector2&& obj) noexcept
+            {
+                if (this == &data)
+                    return *this;
+                
+                this->m_x = std::move(obj.m_x);
+                this->m_y = std::move(obj.m_y);
+                
+                obj.m_x = 0;
+                obj.m_y = 0;
+
+                return *this;
+            }
+
+            /* OPERATORS OVERLOADS */
+            
+
+            /* PUBLIC GETTERS */
+            // Returns the x member variable
+            const U& getX() const noexcept
+            {
+                return m_x;
+            }
+
+            // Returns the y member variable
+            const T& getX() const noexcept
+            {
+                return m_y;
+            }
+
+            /* EXTERNS/OVERRIDES */
+            // Print out the object
+            friend std::ostream& operator<<(std::ostream& out, const Vec2<T,U>& obj)
+            {
+                out << "( " << obj.m_x << ", " << obj.m_y << ")";
+                return out;
+            }
+        private:
+            /* Member variables */
+            T m_x{};
+            U m_y{};
+        }; template<typename T, typename U> using Vec2 = Vector2<T, U>;
     }
 }
 
