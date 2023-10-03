6e067f0824b9a96ab57ad571217fb14706ba02bf
Felix <142559040+filouRE@users.noreply.github.com>
2023-10-02 22:12:05 -0400

    fixed cmake on windows

---
+++ example/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "./reutils.h"
+
+int main()
+{
+    std::string str{ "Hello, World!" };
+    std::cout << str << std::endl;
+    
+    system("pause");
+    return 0;
+}
