7fe3e9ebb0edfc13b15c2b77d0e6b6649b555d07
Félix <142559040+filouRE@users.noreply.github.com>
2023-10-04 13:59:21 -0400

    to fix

--- build.bat
+++ build.bat
@@ -1,3 +1,25 @@
+@echo off
+
+if exist "C:\Program Files (x86)\CMake\bin\" (
+    echo "CMake is already installed."
+    goto build_project
+) else (
+    echo "CMake is not installed."
+    choice /c yn /m "Do you want to install it? "
+    if errorlevel 2 (
+        echo "Exiting the script."
+        exit
+    ) else if errorlevel 1 (
+        :: powershell -Command "Invoke-WebRequest -Uri 'https://github.com/Kitware/CMake/releases/download/v3.27.6/cmake-3.27.6-windows-x86_64.msi' -OutFile 'cmake-install.msi'"
+        msiexec /i .\cmake-install.msi /QN
+        echo "CMake is installed."
+    ) else (
+        echo "Invalid choice."
+        exit
+    )
+)
+
+:build_project
 mkdir build
 cd build
 cmake -G "Visual Studio 17 2022" ..
